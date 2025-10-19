f621f9ad7dbc373658ee549a096c7533bfa59c59
Jeferson Alvarez <mymotre213q@gmail.com>
2025-10-19 10:38:06 -0500

    Eliminar completamente la carpeta backend del repositorio

--- backend/apps/ml/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Módulo ML para CacaoScan.
-
-Este módulo proporciona endpoints específicos para predicciones de ML
-incluyendo YOLOv8 y recorte inteligente estilo iPhone.
-"""
-
-default_app_config = 'apps.ml.apps.MLConfig'
